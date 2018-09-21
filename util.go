758d8870a512179f09559600f0b5828ce494fb7a
Nikita Afanasenko <nikita@afanasenko.name>
2018-09-21 17:57:39 +0300

    Fix Varint serialization

--- util.go
+++ util.go
@@ -31,15 +31,15 @@ func ReverseHex(b []byte) []byte {
 func Varint(n uint64) []byte {
 	if n > 4294967295 {
 		val := make([]byte, 8)
-		binary.BigEndian.PutUint64(val, n)
+		binary.LittleEndian.PutUint64(val, n)
 		return append([]byte{0xFF}, val...)
 	} else if n > 65535 {
 		val := make([]byte, 4)
-		binary.BigEndian.PutUint32(val, uint32(n))
+		binary.LittleEndian.PutUint32(val, uint32(n))
 		return append([]byte{0xFE}, val...)
-	} else if n > 255 {
+	} else if n >= 0xFD {
 		val := make([]byte, 2)
-		binary.BigEndian.PutUint16(val, uint16(n))
+		binary.LittleEndian.PutUint16(val, uint16(n))
 		return append([]byte{0xFD}, val...)
 	} else {
 		return []byte{byte(n)}
